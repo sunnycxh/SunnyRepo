6dd2365f22bc31ba081312f379ef065403ed54d0
Sunny Cui <scui@illumina.com>
2015-10-10 19:15:11 -0700

    Explore the dispose method.

---
+++ MyPractice/TempClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPractice
+{
+    public class TempClass
+    {
+        public int Number = 1;
+        public string Text = "Hehe";
+
+        public TempClass(int number, string text)
+        {
+            Number = number;
+            Text = text;
+        }
+
+        ~TempClass()
+        { }
+    }
+}
